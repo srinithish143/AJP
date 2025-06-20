ec54e1cc11e6f08f8b4913b709e6f82f7afb1c02
srinithish143 <itsmesri2k@gmail.com>
2025-06-20 07:57:34 +0530

    updates

--- 6. Two Way Binding/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('app', [])
-    .controller('FormController', ['$scope', function ($scope) {
-        $scope.user = {}; // Object to store form data
-
-        $scope.submitForm = function () {
-            alert('Form submitted successfully!');
-            console.log($scope.user); // Optional debugging
-        };
-    }]);
